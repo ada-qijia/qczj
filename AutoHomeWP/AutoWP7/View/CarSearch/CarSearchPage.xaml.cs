f4af40bbb6fafd33b3545a049b97dd9e61fd9ed0
elecpiano <elecpiano@gmail.com>
2014-12-11 23:00:39 +0800

    brand list

--- AutoHomeWP/AutoWP7/View/CarSearch/CarSearchPage.xaml.cs
+++ AutoHomeWP/AutoWP7/View/CarSearch/CarSearchPage.xaml.cs
@@ -281,7 +281,8 @@ namespace AutoWP7.View.CarSearch
             }
 
             //http://221.192.136.99:804/wpv1.6/cars/brandsdealer-a2-pm1-v3.0.1-ts635174268324922500.html
-            string url = string.Format("{0}{1}/cars/brandsdealer-{2}-ts{3}.html", App.appUrl, App.versionStr, App.AppInfo, 0);
+            //string url = string.Format("{0}{1}/cars/brandsdealer-{2}-ts{3}.html", App.appUrl, App.versionStr, App.AppInfo, 0);
+            string url = string.Format("{0}{1}/cars/brands-{2}-ts{3}.html", App.appUrl, App.versionStr, App.AppInfo, 0);
             carVM.LoadDataAysnc(url);
         }
 
@@ -308,7 +309,7 @@ namespace AutoWP7.View.CarSearch
 
                 foreach (var entity in groupBy)
                 {
-                    CarBrandGroup group = new CarBrandGroup(entity.key + "         ");
+                    CarBrandGroup group = new CarBrandGroup(entity.key);
 
                     foreach (var item in entity)
                     {
