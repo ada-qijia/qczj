5cfaf2b973a80e5e0dcde2b2abbdaa620f103db1
QianJia <jq9876543210@163.com>
2015-03-02 18:19:58 +0800

    Autohome: Add missed files;

---
+++ AutoHomeWP/AutoWP7/View/Me/CompleteMyInfo.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using AutoWP7.Handler;
+using ViewModels.Handler;
+using Newtonsoft.Json.Linq;
+using Model;
+using System.IO.IsolatedStorage;
+
+namespace AutoWP7.View.Me
+{
+    public partial class CompleteMyInfo : PhoneApplicationPage
+    {
+        private string platformId;
+        private Model.Me.ThirdPartyAccountModel thirdPartyModel;
+
+        public CompleteMyInfo()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (NavigationContext.QueryString.ContainsKey("platformId"))
+            {
+                platformId = NavigationContext.QueryString["platformId"];
+                switch (platformId)
+                {
+                    //weibo
+                    case "16":
+                        var settings = IsolatedStorageSettings.ApplicationSettings;
+                        if (settings.Contains(Utils.MeHelper.weiboAccountKey))
+                        {
+                            thirdPartyModel = settings[Utils.MeHelper.weiboAccountKey] as Model.Me.ThirdPartyAccountModel;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void ConnectAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/View/Me/ConnectAccount.xaml?platformId=" + this.platformId, UriKind.Relative));
+        }
+
+        protected override void OnBackKeyPress(System.ComponentModel.CancelEventArgs e)
+        {
+            Common.showMsg("完善资料失败");
+            base.OnBackKeyPress(e);
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            this.Focus();
+
+            bool checkSuccess = this.ThirdPartyRegisterPanel.CheckData();
+            if (checkSuccess && thirdPartyModel != null)
+            {
+                string username = HttpUtility.UrlEncode(this.ThirdPartyRegisterPanel.UsernameTextbox.Text);
+                string validcode = this.ThirdPartyRegisterPanel.CodeTextbox.Text;
+                string mobile = this.ThirdPartyRegisterPanel.PhoneNoTextBox.Text;
+                int expiresin = (thirdPartyModel.ExpiresIn - DateTime.Now).Seconds;
+                string ua = AutoWP7.Handler.Common.GetAutoHomeUA();
+
+                string format = "_appid={0}&autohomeua={1}&_timeStamp={2}&token={3}&openId={4}&username={5}&validcode={6}&mobile={7}&platformid={8}&expiresin={9}";//&refreshtoken={10}&area={11}
+                string data = string.Format(format, Utils.MeHelper.appIDWp, ua, Common.GetTimeStamp(), thirdPartyModel.AccessToken, thirdPartyModel.OpenId, username, validcode, mobile, this.platformId, expiresin);
+                data = Common.SortURLParamAsc(data);
+                string sign = Common.GetSignStr(data);
+                data += "&_sign=" + sign;
+
+                GlobalIndicator.Instance.Text = "正在提交...";
+                GlobalIndicator.Instance.IsBusy = true;
+
+                string url = Utils.MeHelper.ThirdPartyRegisterUrl;
+                var upstreamVM = ViewModels.Me.UpStreamViewModel.SingleInstance;
+                upstreamVM.UploadAsync(url, data, wc_UploadStringCompleted);
+            }
+        }
+
+        private void wc_UploadStringCompleted(object sender, UploadStringCompletedEventArgs ee)
+        {
+            GlobalIndicator.Instance.Text = null;
+            GlobalIndicator.Instance.IsBusy = false;
+
+            try
+            {
+                if (ee.Error is WebException)
+                {
+                    Common.showMsg("当前网络不可用，请检查网络设置");
+                }
+                else if (ee.Error == null && ee.Cancelled == false)
+                {
+                    JObject json = JObject.Parse(ee.Result);
+                    int resultCode = (int)json.SelectToken("returncode");
+                    string message = json.SelectToken("message").ToString();
+                    switch (resultCode)
+                    {
+                        case 0://成功
+                            JToken resultToken = json.SelectToken("result");
+                            if (resultToken != null)
+                            {
+                                int userID = resultToken.SelectToken("userid").Value<int>();
+                                string mobile = resultToken.SelectToken("mobilephone").ToString();
+                                string auth = resultToken.SelectToken("pcpopclub").ToString();
+                                string nickName = resultToken.SelectToken("nickname").ToString();
+
+                                //存入文件
+                                var model = new MyForumModel();
+                                model.Success = 1;
+                                model.Message = message;
+                                model.UserID = userID;
+                                model.UserName = nickName;
+                                model.Authorization = auth;
+
+                                var setting = IsolatedStorageSettings.ApplicationSettings;
+                                setting["userInfo"] = model;
+                                setting.Save();
+
+                                //返回登录页
+                                Common.showMsg("完善资料成功");
+                                NavigationService.GoBack();
+                            }
+                            break;
+                        default://其他情况...
+                            if (!string.IsNullOrEmpty(message))
+                            {
+                                Common.showMsg(message);
+                            }
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ AutoHomeWP/AutoWP7/View/Me/ConnectAccount.xaml.cs
@@ -0,0 +1,111 @@
+﻿using AutoWP7.Handler;
+using Microsoft.Phone.Controls;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Windows.Navigation;
+using ViewModels.Me;
+
+namespace AutoWP7.View.Me
+{
+    public partial class ConnectAccount : PhoneApplicationPage
+    {
+        private string platformId;
+        private Model.Me.ThirdPartyAccountModel thirdPartyModel;
+
+        public ConnectAccount()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (NavigationContext.QueryString.ContainsKey("platformId"))
+            {
+                platformId = NavigationContext.QueryString["platformId"];
+                switch (platformId)
+                {
+                    //weibo
+                    case "16":
+                        var settings = System.IO.IsolatedStorage.IsolatedStorageSettings.ApplicationSettings;
+                        if (settings.Contains(Utils.MeHelper.weiboAccountKey))
+                        {
+                            thirdPartyModel = settings[Utils.MeHelper.weiboAccountKey] as Model.Me.ThirdPartyAccountModel;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            bool passwordEmpty = string.IsNullOrEmpty(this.PasswordTextbox.Password);
+            if (string.IsNullOrEmpty(this.UsernameTextbox.Text))
+            {
+                string message = passwordEmpty ? "请您输入账号密码" : "请您输入账户名";
+                Common.showMsg(message);
+            }
+            else if (passwordEmpty)
+            {
+                Common.showMsg("请您输入密码");
+            }
+            else
+            {
+                string username = this.UsernameTextbox.Text;
+                string password = Handler.MD5.GetMd5String(this.PasswordTextbox.Password);
+                //关联账号
+                string format = "_appid={0}&_timeStamp={1}&autohomeua={2}&logincode={3}&userPwd={4}&openId={5}&platformid={6}&token={7}&tokenSecret={8}&orginalName={9}";
+                string data = string.Format(format, Utils.MeHelper.appID, Common.GetTimeStamp(), AutoWP7.Handler.Common.GetAutoHomeUA(), username, password, thirdPartyModel.OpenId, this.platformId, thirdPartyModel.AccessToken, string.Empty, string.Empty);
+                data = Common.SortURLParamAsc(data);
+                string sign = Common.GetSignStr(data);
+                data += "&_sign=" + sign;
+
+                UpStreamViewModel upstreamVM = UpStreamViewModel.SingleInstance;
+                upstreamVM.UploadAsync(Utils.MeHelper.ConnectAccountUrl, data, wc_UploadStringCompleted);
+            }
+        }
+
+        void wc_UploadStringCompleted(object sender, UploadStringCompletedEventArgs e)
+        {
+            try
+            {
+                if (e.Error is WebException)
+                {
+                    Common.showMsg("当前网络不可用，请检查网络设置");
+                }
+                else if (e.Error == null && e.Cancelled == false)
+                {
+                    JObject json = JObject.Parse(e.Result);
+                    int resultCode = (int)json.SelectToken("returncode");
+                    string message = json.SelectToken("message").ToString();
+                    //成功
+                    if (resultCode == 0)
+                    {
+                        JToken resultToken = json.SelectToken("result");
+                        if (resultToken != null)
+                        {
+                            int userID = resultToken.SelectToken("UserId").Value<int>();
+                            int userState = resultToken.SelectToken("UserState").Value<int>();
+                            string auth = resultToken.SelectToken("PcpopClub").ToString();
+                            string nickName = resultToken.SelectToken("LoginCode").ToString();
+
+                            //返回登录前页面
+                            for (int i = 0; i < 3; i++)
+                            {
+                                NavigationService.RemoveBackEntry();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        string msg = string.IsNullOrEmpty(message) ? "关联失败" : message;
+                        Common.showMsg(msg);
+                    }
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
