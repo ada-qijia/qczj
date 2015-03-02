4bad31980554711745aaa9df2582e685582fade2
elecpiano <elecpiano@gmail.com>
2015-03-02 17:27:59 +0800

    video rollback

--- AutoHomeWP/AutoWP7/View/Channel/News/VideoEndPage.xaml.cs
+++ AutoHomeWP/AutoWP7/View/Channel/News/VideoEndPage.xaml.cs
@@ -141,6 +141,7 @@ namespace AutoWP7.View.Channel.News
             WebBrowserTask webBrowserTask = new WebBrowserTask();
             //http://221.192.136.99:804/wpv1.5/content/news/wpvideopage-a2-pm3-v1.6.0-vidXNzkyMzczMjI4.html
             string url = string.Format("{0}{1}/content/news/wpvideopage-{2}-vid{3}.html", App.newsPageDomain, App.versionStr, App.AppInfo, videoData.YoukuVideoKey);
+            //string url = string.Format("http://player.youku.com/embed/{0}", videoData.YoukuVideoKey);
 
             webBrowserTask.Uri = new Uri(url, UriKind.RelativeOrAbsolute);
             webBrowserTask.Show();
